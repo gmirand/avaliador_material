8ed8dc38f015368772335307e95cc164e6500a85
Guilherme Miranda Costa <gmiranda.phn@gmail.com>
2024-04-01 16:46:28 -0300

    Update 1_Analisador.py

--- pages/1_Analisador.py
+++ pages/1_Analisador.py
@@ -13,7 +13,7 @@ def to_excel(df):
     """
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        df.to_excel(writer, index=False, sheet_name='Sheet1', decimal=',')
     output.seek(0)
     return output.getvalue()
 
